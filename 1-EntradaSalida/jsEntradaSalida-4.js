fa4e5321d05443287b08221b72fef66c0385ac8d
Tomás Suriani <32534018+tsuriani@users.noreply.github.com>
2020-02-06 22:20:59 -0300

    Laboratorio 6 Clase 1

--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -1,9 +1,11 @@
 /* 
-	Debemos lograr tomar un dato por 'PROMPT' 
-	y lo muestro por 'getElementById' al presionar el botón 'mostrar'
+Debemos lograr tomar un dato por 'PROMPT' 
+y lo muestro por 'getElementById' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-
+	var nombre;
+	nombre = prompt("Ingrese su nombre");
+	document.getElementById("elNombre").value = nombre;
 }
 
