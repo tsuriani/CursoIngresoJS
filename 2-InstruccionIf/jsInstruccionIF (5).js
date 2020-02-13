ed745b23e7ebaade49ba57518c6d2e2f5982484a
Tomás Suriani <32534018+tsuriani@users.noreply.github.com>
2020-02-13 20:45:31 -0300

    Laboratorio 6 Clase 3

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,11 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
+edad = document.getElementById("edad").value;
 
-
+if (edad < 13 || edad > 17) {
+    //(!(edad < 13 && edad > 17))
+    alert("Persona no adolescente");
+}
 }//FIN DE LA FUNCIÓN
