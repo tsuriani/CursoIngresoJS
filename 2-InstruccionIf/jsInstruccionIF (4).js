07c060247e9367d8f96d31ef3b28964bd22a9218
Tomás Suriani <32534018+tsuriani@users.noreply.github.com>
2020-02-13 21:28:10 -0300

    Laboratorio 6 Clase 3

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -5,9 +5,9 @@ var edad;
 edad = document.getElementById("edad").value;
 
 if (if (edad > 12 && edad < 18) { 
-//(!(edad < 12 || edad > 18))
-//Operador lógico "&&" AND/ampersand   
-//Operador lógico "||" OR/barra vertical    
+    //(!(edad < 12 || edad > 18))
+    //Operador lógico "&&" AND/ampersand   
+    //Operador lógico "||" OR/barra vertical    
     alert("Persona adolescente");
     }
 }//FIN DE LA FUNCIÓN
