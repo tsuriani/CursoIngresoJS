58c5898079805279676f93148bf9cc3ac239d8f3
Tomás Suriani <32534018+tsuriani@users.noreply.github.com>
2020-02-14 20:41:50 -0300

    Laboratorio 6 Clase 4

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,16 @@
 function mostrar()
-{
-	//Genero el número RANDOM entre 1 y 10 
-	
+{//Genero el número RANDOM entre 1 y 10 
+	var numero;
+	var maximo = 10;
+	var minimo = 1;
+	numero = Math.floor(Math.random()*((maximo + 1) - minimo) + minimo);
+	alert(numero);
 
+	if (numero >= 9) {
+		alert("Excelente");
+	} else if (numero >= 4 && numero <= 8) {
+		alert("Aprobó");
+	} else {
+		alert("Vamos, la proxima se puede");
+	}
 }//FIN DE LA FUNCIÓN
