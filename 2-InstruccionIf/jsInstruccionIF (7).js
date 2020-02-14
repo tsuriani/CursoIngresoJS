64a0c50cc3236ce1cd168a6e4845485e922c3817
Tomás Suriani <32534018+tsuriani@users.noreply.github.com>
2020-02-13 21:50:45 -0300

    Laboratorio 6 Clase 3

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,8 +1,11 @@
 function mostrar()
-{
-//tomo la edad  
-
+{//tomo la edad 	
+	var edad;
+	edad = document.getElementById("edad").value;
+    	var estadoCivil;
+    	estadoCivil = document.getElementById("estadoCivil").value
 	
-
-
+	if (edad < 18 && (estadoCivil != "Soltero")) {
+		 alert("Es muy pequeño para no ser soltero");
+    }
 }//FIN DE LA FUNCIÓN
