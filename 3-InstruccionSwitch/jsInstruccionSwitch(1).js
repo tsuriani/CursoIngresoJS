fc477e1c358343ee0a8fe66646dab30d2765106f
Tomás Suriani <32534018+tsuriani@users.noreply.github.com>
2020-02-14 20:48:46 -0300

    Laboratorio 6 Clase 4

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -2,8 +2,20 @@ function mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
+  switch (mesDelAño) {
+      case "Enero":
+      alert("Que comience bien el año");
+      break;
 
+      case "Marzo":
+      alert("A clases");
+      break;
 
+      case "Julio":
+      alert("Se vienen las vacaciones");
+      break;
 
-
+      case "Diciembre":
+      alert("Felices fiestas");
+      break;
 }//FIN DE LA FUNCIÓN
